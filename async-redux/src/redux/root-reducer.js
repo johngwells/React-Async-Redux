cdf09fc92d5a626d54fb2cd684bc361fc1c916e2
John Wells <molekewl2017@gmail.com>
2019-10-13 18:54:23 -0700

    add logic to reducers

--- async-redux/src/redux/root-reducer.js
+++ async-redux/src/redux/root-reducer.js
@@ -1,9 +1,33 @@
-export const  INITIAL_STATE = {
+import { START_FETCHING, FETCH_SUCCESS, FETCH_FAILURE } from './actions/index';
+
+export const INITIAL_STATE = {
   // initial state here
+  magic: [],
+  isFetching: false,
+  error: ''
 }
 
 const useReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
+    case START_FETCHING:
+      return {
+        ...state,
+        isFetching: true,
+        error: ''
+      }
+    case FETCH_SUCCESS:
+      return {
+        ...state,
+        isFetching: false,
+        error: '',
+        magic: action.payload
+      }
+    case FETCH_FAILURE:
+      return {
+        ...state,
+        isFetching: false,
+        error: action.payload
+      }
     default:
       return state
   }
