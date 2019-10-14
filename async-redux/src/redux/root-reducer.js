b91ef39f9668ea838ba753898e55cb28516da9f7
John Wells <molekewl2017@gmail.com>
2019-10-13 18:28:50 -0700

    root-reducer

---
+++ async-redux/src/redux/root-reducer.js
@@ -0,0 +1,12 @@
+export const  INITIAL_STATE = {
+  // initial state here
+}
+
+const useReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
+
+export default useReducer;
