24f46af3f115e98ee3546b5fdf0bcac919712ce4
John Wells <molekewl2017@gmail.com>
2019-10-13 20:29:58 -0700

    basic css

--- async-redux/src/components/MagicCard.js
+++ async-redux/src/components/MagicCard.js
@@ -2,10 +2,17 @@ import React from 'react';
 
 const MagicCard = ({ magic }) => {
   return (
-    <>
-      <p>{magic[0].name}</p>
-      <img src={magic[0].imageUrl} alt={magic.name} />
-    </>
+    <div className='magic-container'>
+      {Object.values(magic).map(card => (
+        <div key={card.id} className='card-container'>
+          <img src={card.imageUrl} alt={card.name} />
+          <p>set: {card.setName}</p>
+          <p>name: {card.name}</p>
+          <p>color: {card.color}</p>
+          <p>text: {card.text}</p>
+        </div>
+      ))}
+    </div>
   )
 }
 
