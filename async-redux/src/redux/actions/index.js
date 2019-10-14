81fc28f4d018af5a16724af8fb224fe9dac3ba72
John Wells <molekewl2017@gmail.com>
2019-10-13 18:39:33 -0700

    create actions

---
+++ async-redux/src/redux/actions/index.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+// action types
+export const START_FETCHING = 'START_FETCHING';
+export const FETCH_SUCCESS = 'FETCH_SUCCESS';
+export const FETCH_FAILURE = 'FETCH_FAILURE';
+
+// action creator
+export const fetchData = () => dispatch => {
+  // action objects
+  dispatch({ type: START_FETCHING });
+
+  axios
+    .get('https://api.magicthegathering.io/v1/cards')
+    .then(res => dispatch({ type: FETCH_SUCCESS, payload: res.data }))
+    .catch(err => dispatch({ type: FETCH_FAILURE, payload: err.res }))
+}
